788616c8db933ec9cef1aee05c3a36499eccd747
Nicholas Devenish <ndevenish@gmail.com>
2017-11-20 16:35:21 +0000

    Remove old autogen file

--- do_autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env sh
-
-autogen/resolve.py autogen/bootstrap.log autogen/autogen.yaml --target=autogen/md
-autogen/depsToCMake.py autogen/md/AutoBuildDeps.yaml --target=cmakelists --override=autogen/cmake_templates/
-
