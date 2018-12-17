67f4548a3903b09a1c6b6ba06f6973113c798f11
Nicholas Devenish <ndevenish@gmail.com>
2018-12-17 12:45:32 +0000

    Black, flake8 and pre-commit

--- prepare_singlemodule.py
+++ prepare_singlemodule.py
@@ -15,7 +15,7 @@ Options:
                     repository is assumed to exist as a subdirectory of this.
 
 Designed to make travis-based testing intuitive (e.g. working with updates
-directly on the individual repositories). This means you can check out a 
+directly on the individual repositories). This means you can check out a
 single repository (at whatever branch or pull request it comes from) and
 have the rest of the build automatically constructed around it.
 
@@ -26,90 +26,93 @@ Run this in the root of your 'module' directory, with:
 
 from __future__ import print_function
 
-import os, sys
+import os
 import subprocess
 import shutil
 
 import docopt
 
+
 def merge_tree(src, dst):
-  """Copy all files in source tree to destination, merging with an existing tree."""
-  for path, dirs, files in os.walk(src):
-    relpath = path[len(src)+1:]
-    # Copy files over
-    for file in files:
-      fullsrcpath = os.path.join(path, file)
-      fulldstpath = os.path.join(dst, relpath, file)
-      shutil.copy2(fullsrcpath, fulldstpath)
-    # Ensure the subdirectories exist
-    for dir in dirs:
-      fulldstpath = os.path.join(dst, relpath, dir)
-      if not os.path.exists(fulldstpath):
-        print("Making ", fulldstpath)
+    """Copy all files in source tree to destination, merging with existing tree."""
+    for path, dirs, files in os.walk(src):
+        relpath = path[len(src) + 1 :]
+        # Copy files over
+        for file in files:
+            fullsrcpath = os.path.join(path, file)
+            fulldstpath = os.path.join(dst, relpath, file)
+            shutil.copy2(fullsrcpath, fulldstpath)
+        # Ensure the subdirectories exist
+        for dir in dirs:
+            fulldstpath = os.path.join(dst, relpath, dir)
+            if not os.path.exists(fulldstpath):
+                print("Making ", fulldstpath)
+
 
 def get_commit_id(folder):
-  """Returns the current git commit ID of a repository, given it's working dir"""
-  newenv = os.environ.copy()
-  newenv["GIT_DIR"] = os.path.join(folder, ".git")
-  ret = subprocess.check_output(["git", "rev-parse", "HEAD"], env=newenv)
-  assert len(ret.strip().splitlines()) == 1
-  return ret.strip()
+    """Returns the current git commit ID of a repository, given it's working dir"""
+    newenv = os.environ.copy()
+    newenv["GIT_DIR"] = os.path.join(folder, ".git")
+    ret = subprocess.check_output(["git", "rev-parse", "HEAD"], env=newenv)
+    assert len(ret.strip().splitlines()) == 1
+    return ret.strip()
+
 
 # Map of folder names to repository locations
 # Either "URL String" or ["URL String", "and_list", "of", "arguments"]
 repositories = {
-  "dials":          "https://github.com/dials/dials.git",
-  "cctbx_project":  "https://github.com/cctbx/cctbx_project.git",
-  "cbflib":         "https://github.com/yayahjb/cbflib.git",
-  "ccp4io":         "https://github.com/dials/ccp4io.git",
-  "xia2":           "https://github.com/xia2/xia2.git",
-  "annlib_adaptbx": "https://github.com/dials/annlib_adaptbx.git",
-  "annlib":         "https://github.com/dials/annlib.git",
-  "ccp4io_adaptbx": "https://github.com/dials/ccp4io_adaptbx.git",
-  "tntbx":          "https://github.com/dials/tntbx.git",
-  "gui_resources":  "https://github.com/dials/gui_resources.git",
+    "dials": "https://github.com/dials/dials.git",
+    "cctbx_project": "https://github.com/cctbx/cctbx_project.git",
+    "cbflib": "https://github.com/yayahjb/cbflib.git",
+    "ccp4io": "https://github.com/dials/ccp4io.git",
+    "xia2": "https://github.com/xia2/xia2.git",
+    "annlib_adaptbx": "https://github.com/dials/annlib_adaptbx.git",
+    "annlib": "https://github.com/dials/annlib.git",
+    "ccp4io_adaptbx": "https://github.com/dials/ccp4io_adaptbx.git",
+    "tntbx": "https://github.com/dials/tntbx.git",
+    "gui_resources": "https://github.com/dials/gui_resources.git",
 }
 options = docopt.docopt(__doc__)
 
 commit_ids = {}
 for name, url in repositories.items():
-  # Assume that if the path exists at all, the user knows what they are doing
-  if not os.path.exists(name):
-    command = ["git", "clone"]
-    # Convert to a list if not one already (allows multiple custom parameters)
-    if isinstance(url, basestring):
-      url = [url]
+    # Assume that if the path exists at all, the user knows what they are doing
+    if not os.path.exists(name):
+        command = ["git", "clone"]
+        # Convert to a list if not one already (allows multiple custom parameters)
+        if isinstance(url, basestring):
+            url = [url]
 
-    if options["--shallow"]:
-      command.append("--depth=1")
-    
-    # If the reference path exists, pass that through
-    if options["--reference"]:
-      ref_path = os.path.join(options["--reference"], name)
-      ref_git = os.path.join(options["--reference"], name)
-      if os.path.isdir(ref_path):
-        command.append("--reference-if-able={}".format(ref_path))
+        if options["--shallow"]:
+            command.append("--depth=1")
 
-    command.extend(url)
-    print("Running:", " ".join(command))
-    subprocess.check_call(command)
-  else:
-    print("{} folder exists, skipping.".format(name))
+        # If the reference path exists, pass that through
+        if options["--reference"]:
+            ref_path = os.path.join(options["--reference"], name)
+            ref_git = os.path.join(options["--reference"], name)
+            if os.path.isdir(ref_path):
+                command.append("--reference-if-able={}".format(ref_path))
 
-  if options["--write-log"]:
-    commit_ids[name] = get_commit_id(name)
+        command.extend(url)
+        print("Running:", " ".join(command))
+        subprocess.check_call(command)
+    else:
+        print("{} folder exists, skipping.".format(name))
+
+    if options["--write-log"]:
+        commit_ids[name] = get_commit_id(name)
 
 if options["--write-log"]:
-  with open("commit_ids.txt", "wt") as f:
-    maxlen = max(len(x) for x in commit_ids)
-    for name, sha in sorted(commit_ids.items(), key=lambda (x,y): x):
-      f.write(name.ljust(maxlen) + " " + sha + "\n")
+    with open("commit_ids.txt", "wt") as f:
+        maxlen = max(len(x) for x in commit_ids)
+        for name, sha in sorted(commit_ids.items(), key=lambda x, y: x):
+            f.write(name.ljust(maxlen) + " " + sha + "\n")
 
 if not options["--no-cmake"]:
-  # Copy over tree of files from cmake
-  merge_tree("cmake/cmakelists", os.getcwd())
-  try:
-    shutil.copy2("cmake/RootCMakeLists.txt", "CMakeLists.txt")
-  except shutil.Error:
-    # Happens if both files are the same
-    pass
+    # Copy over tree of files from cmake
+    merge_tree("cmake/cmakelists", os.getcwd())
+    try:
+        shutil.copy2("cmake/RootCMakeLists.txt", "CMakeLists.txt")
+    except shutil.Error:
+        # Happens if both files are the same
+        pass
--- write_libtbx_env.py
+++ write_libtbx_env.py
@@ -15,96 +15,117 @@ import pickle
 import sys
 from types import ModuleType
 
+
 def new_module(name, doc=None):
-  """Create a new module and inject it into sys.modules"""
-  m = ModuleType(name, doc)
-  m.__file__ = name + '.py'
-  sys.modules[name] = m
-  return m
+    """Create a new module and inject it into sys.modules"""
+    m = ModuleType(name, doc)
+    m.__file__ = name + ".py"
+    sys.modules[name] = m
+    return m
+
 
 # Create the fake libtbx environment variables
 libtbx = new_module("libtbx")
 libtbx.env_config = new_module("libtbx.env_config")
 libtbx.path = new_module("libtbx.path")
 
+
 def into(module):
-  """Decorates a class to look like it came from a specific module."""
-  def _wrap(classdef):
-    setattr(module, classdef.__name__, classdef)
-    classdef.__module__ = module.__name__
-    return classdef
-  return _wrap
+    """Decorates a class to look like it came from a specific module."""
+
+    def _wrap(classdef):
+        setattr(module, classdef.__name__, classdef)
+        classdef.__module__ = module.__name__
+        return classdef
+
+    return _wrap
+
 
 @into(libtbx.path)
 class path_mixin(object):
-  pass
+    pass
+
 
 @into(libtbx.path)
 class relocatable_path(path_mixin):
-  """Replicate the relocatable_path attributes"""
-  def __init__(self, anchor, relative):
-    self._anchor = absolute_path(anchor)
-    # Replicate the logic that recalculates relativity if necessary
-    if os.path.isabs(relative):
-      relative = os.path.relpath(relative, self._anchor._path)
-    self.relocatable = relative
+    """Replicate the relocatable_path attributes"""
+
+    def __init__(self, anchor, relative):
+        self._anchor = absolute_path(anchor)
+        # Replicate the logic that recalculates relativity if necessary
+        if os.path.isabs(relative):
+            relative = os.path.relpath(relative, self._anchor._path)
+        self.relocatable = relative
+
 
 @into(libtbx.path)
 class absolute_path(path_mixin):
-  """Replicate the absolute_path attributes"""
-  def __init__(self, path):
-    if isinstance(path, absolute_path):
-      path = path._path
-    self._path = os.path.abspath(path)
+    """Replicate the absolute_path attributes"""
+
+    def __init__(self, path):
+        if isinstance(path, absolute_path):
+            path = path._path
+        self._path = os.path.abspath(path)
+
 
 @into(libtbx.env_config)
 class build_options:
-  """Replicate an empty build_options"""
-  pass
+    """Replicate an empty build_options"""
+
+    pass
+
 
 @into(libtbx.env_config)
 class environment:
-  """Replicate the pickled environment object"""
-  def __init__(self, build_path, modules):
-    # Used for .under_build, .under_base, relocatability
-    self.build_path = absolute_path(build_path)
-    # Needed for env_config.unpickle compatibility - defaults
-    # will be written but this is only(?) used during build
-    self.build_options = build_options()
-    # Checked for existence as part of unpickling compatibility
-    self.relocatable = True
-    # Used during loading to set custom environment variables
-    self.module_list = []
-    # Used on load to cross-check configurations
-    self.python_version_major_minor = sys.version_info[:2]
+    """Replicate the pickled environment object"""
 
-    # Dictionary used for looking up dist paths
-    self.module_dist_paths = {name: relocatable_path(build_path, path) for name, path in modules}
+    def __init__(self, build_path, modules):
+        # Used for .under_build, .under_base, relocatability
+        self.build_path = absolute_path(build_path)
+        # Needed for env_config.unpickle compatibility - defaults
+        # will be written but this is only(?) used during build
+        self.build_options = build_options()
+        # Checked for existence as part of unpickling compatibility
+        self.relocatable = True
+        # Used during loading to set custom environment variables
+        self.module_list = []
+        # Used on load to cross-check configurations
+        self.python_version_major_minor = sys.version_info[:2]
 
-    # Work out the repository locations by working backwards from libtbx
-    tbx_rel_path = self.module_dist_paths["libtbx"]
-    tbx_path = os.path.normpath(os.path.join(tbx_rel_path._anchor._path, tbx_rel_path.relocatable))
-    cctbx_path = os.path.dirname(tbx_path)
-    modules_path = os.path.dirname(cctbx_path)
+        # Dictionary used for looking up dist paths
+        self.module_dist_paths = {
+            name: relocatable_path(build_path, path) for name, path in modules
+        }
 
-    # So that libtbx.env_config.find_in_repositories works
-    self.repository_paths = [
-      relocatable_path(self.build_path, modules_path),
-      relocatable_path(self.build_path, cctbx_path),
-    ]
+        # Work out the repository locations by working backwards from libtbx
+        tbx_rel_path = self.module_dist_paths["libtbx"]
+        tbx_path = os.path.normpath(
+            os.path.join(tbx_rel_path._anchor._path, tbx_rel_path.relocatable)
+        )
+        cctbx_path = os.path.dirname(tbx_path)
+        modules_path = os.path.dirname(cctbx_path)
+
+        # So that libtbx.env_config.find_in_repositories works
+        self.repository_paths = [
+            relocatable_path(self.build_path, modules_path),
+            relocatable_path(self.build_path, cctbx_path),
+        ]
+
 
 # Extract the lists of modules, paths from arguments
-paths = list(sorted(zip(sys.argv[1].split(";"), sys.argv[2].split(";")), key=lambda x: x[0]))
+paths = list(
+    sorted(zip(sys.argv[1].split(";"), sys.argv[2].split(";")), key=lambda x: x[0])
+)
 build_path = os.getcwd()
 
-maxl = max([len(x) for x, y in paths]+[6])
+maxl = max([len(x) for x, y in paths] + [6])
 print("Writing libtbx_env")
 print("Build path: {}".format(build_path))
 print("Module".ljust(maxl) + " Path")
 for name, path in paths:
-  print ("{} {}".format(name.ljust(maxl), path))
+    print("{} {}".format(name.ljust(maxl), path))
 
 a = environment(build_path, paths)
 
 with open("libtbx_env", "w") as f:
-  pickle.dump(a, f)
+    pickle.dump(a, f)
